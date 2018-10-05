aa488433cd98a522323605a20456a50ec97d9d69
franpog859 <franciszek.pog@gmail.com>
2018-10-05 21:27:58 +0200

    Fix incorrect function exporting and add tests

--- index.go
+++ index.go
@@ -1,12 +1,38 @@
 package index
 
-func getAll(slice []interface{}, _item interface{}) (indexes []int, ok bool) {
+import (
+	"github.com/pkg/errors"
+	"reflect"
+)
+
+func GetAll(_slice interface{}, _item interface{}) (indexes []int, err error) {
 	indexes = []int{}
+
+	slice, err := sliceFromInterface(_slice)
+	if err != nil {
+		return indexes, err
+	}
+
 	for index, item := range slice {
 		if item == _item {
 			indexes = append(indexes, index)
 		}
 	}
 
-	return indexes, len(indexes) != 0
+	return indexes, nil
+}
+
+func sliceFromInterface(sliceInterface interface{}) (r0 []interface{}, err error) {
+	slice := reflect.ValueOf(sliceInterface)
+	if slice.Kind() != reflect.Slice {
+		return nil, errors.New("index: given non-slice type")
+	}
+
+	r0 = make([]interface{}, slice.Len())
+
+	for i := 0; i < slice.Len(); i++ {
+		r0[i] = slice.Index(i).Interface()
+	}
+
+	return r0, nil
 }
--- index_test.go
+++ index_test.go
@@ -8,16 +8,68 @@ import (
 )
 
 func TestIndex_GetAll(t *testing.T) {
-	t.Run("asda", func(t *testing.T) {
+	t.Run("returns proper indexes", func(t *testing.T) {
 		//given
 		slice := []int{1, 2, 3, 1}
 		item := 1
 
 		//when
-		indexes, ok := index.getAll(slice, item)
+		indexes, err := index.GetAll(slice, item)
 
 		//then
-		require.Equal(t, true, ok)
+		require.Equal(t, nil, err)
 		assert.Equal(t, []int{0, 3}, indexes)
+	})
+
+	t.Run("returns empty slice if no items match the template", func(t *testing.T) {
+		//given
+		slice := []int{1, 2, 3, 1}
+		item := 5
+
+		//when
+		indexes, err := index.GetAll(slice, item)
+
+		//then
+		require.Equal(t, nil, err)
+		assert.Equal(t, []int{}, indexes)
+	})
+
+	t.Run("returns empty slice if given slice is empty", func(t *testing.T) {
+		//given
+		slice := []int{}
+		item := 4
+
+		//when
+		indexes, err := index.GetAll(slice, item)
+
+		//then
+		require.Equal(t, nil, err)
+		assert.Equal(t, []int{}, indexes)
+	})
+
+	t.Run("returns an error if first argument is not a slice", func(t *testing.T) {
+		//given
+		slice := "not a slice"
+		item := 4
+
+		//when
+		indexes, err := index.GetAll(slice, item)
+
+		//then
+		require.Equal(t, "index: given non-slice type", err.Error())
+		assert.Equal(t, []int{}, indexes)
+	})
+
+	t.Run("returns empty slice if the template is different type", func(t *testing.T) {
+		//given
+		slice := []int{1, 2, 3}
+		item := "not an int"
+
+		//when
+		indexes, err := index.GetAll(slice, item)
+
+		//then
+		require.Equal(t, nil, err)
+		assert.Equal(t, []int{}, indexes)
 	})
 }
