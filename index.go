ddcd768a5482e24982544d12fddec10477843ad9
Franciszek Pogodziński <32485355+franpog859@users.noreply.github.com>
2018-10-19 20:18:28 +0200

    Add IsAny and HowMany functions to the package (#2)
    * Add IsAny and HowMany functions to the index
    
    * Add tests for IsAny and HowMany functions
    
    * Provide package description for godoc
    
    * Add tests and USAGE.md file

--- index.go
+++ index.go
@@ -1,3 +1,4 @@
+// Package index provides high level generic like functions for simple slices.
 package index
 
 import (
@@ -10,14 +11,22 @@ import (
 // is positioned in the slice. First argument is the slice in which
 // you are searching for your item and the second one is the item.
 func GetAll(_slice interface{}, _item interface{}) (indexes []int, err error) {
-	indexes = []int{}
+	defer func() {
+		if recover() != nil {
+			err = errors.New("index: incorrect usage of the package")
+		}
+	}()
 
 	slice, err := sliceFromInterface(_slice)
 	if err != nil {
-		return indexes, err
+		return []int{}, err
 	}
 
+	indexes = []int{}
 	for index, item := range slice {
+		if err := checkItemComplexity(item); err != nil {
+			return []int{}, err
+		}
 		if item == _item {
 			indexes = append(indexes, index)
 		}
@@ -26,17 +35,52 @@ func GetAll(_slice interface{}, _item interface{}) (indexes []int, err error) {
 	return indexes, nil
 }
 
-func sliceFromInterface(sliceInterface interface{}) (r0 []interface{}, err error) {
+func sliceFromInterface(sliceInterface interface{}) (ret []interface{}, err error) {
 	slice := reflect.ValueOf(sliceInterface)
 	if slice.Kind() != reflect.Slice {
 		return nil, errors.New("index: given non-slice type")
 	}
 
-	r0 = make([]interface{}, slice.Len())
-
+	ret = make([]interface{}, slice.Len())
 	for i := 0; i < slice.Len(); i++ {
-		r0[i] = slice.Index(i).Interface()
+		ret[i] = slice.Index(i).Interface()
 	}
 
-	return r0, nil
+	return ret, nil
+}
+
+func checkItemComplexity(_item interface{}) (err error) {
+	item := reflect.ValueOf(_item)
+	if item.Kind() == reflect.Slice {
+		return errors.New("index: given multidimensional slice")
+	}
+	if item.Kind() == reflect.Map {
+		return errors.New("index: given too complex slice of maps")
+	}
+
+	return nil
+}
+
+// IsAny function allows you to check if there is at least one item
+// in the slice that you gave. First argument is the slice in which
+// you are searching for your item and the second one is the item.
+func IsAny(_slice interface{}, _item interface{}) (isAny bool, err error) {
+	indexes, err := GetAll(_slice, _item)
+	if err != nil {
+		return false, err
+	}
+
+	return len(indexes) > 0, nil
+}
+
+// HowMany function allows you to check how many items that you gave
+// exist in the slice. First argument is the slice in which
+// you are searching for your items and the second one is the item.
+func HowMany(_slice interface{}, _item interface{}) (howMany int, err error) {
+	indexes, err := GetAll(_slice, _item)
+	if err != nil {
+		return 0, err
+	}
+
+	return len(indexes), nil
 }
--- index_test.go
+++ index_test.go
@@ -120,4 +120,127 @@ func TestIndex_GetAll(t *testing.T) {
 		require.Equal(t, nil, err)
 		assert.Equal(t, []int{0, 2}, indexes)
 	})
+
+	t.Run("returns an error if the given slice is multidimensional", func(t *testing.T) {
+		//given
+		slice := []([]int){{1, 2}, {3}}
+		item := []int{3}
+
+		//when
+		indexes, err := index.GetAll(slice, item)
+
+		//then
+		require.Equal(t, "index: given multidimensional slice", err.Error())
+		assert.Equal(t, []int{}, indexes)
+	})
+
+	t.Run("returns an error if the given slice is multidimensional", func(t *testing.T) {
+		//given
+		slice := make([]map[string]int, 1, 1)
+		item := make(map[string]int)
+		item["key"] = 1
+		slice[0] = item
+
+		//when
+		indexes, err := index.GetAll(slice, item)
+
+		//then
+		require.Equal(t, "index: given too complex slice of maps", err.Error())
+		assert.Equal(t, []int{}, indexes)
+	})
+}
+
+func TestIndex_IsAny(t *testing.T) {
+	t.Run("returns true if item exists in the slice", func(t *testing.T) {
+		//given
+		slice := []int{1, 2, 3, 1}
+		item := 1
+
+		//when
+		isAny, err := index.IsAny(slice, item)
+
+		//then
+		require.Equal(t, nil, err)
+		assert.Equal(t, true, isAny)
+	})
+
+	t.Run("returns false if item does not exist in the slice", func(t *testing.T) {
+		//given
+		slice := []int{1, 2, 3, 1}
+		item := "not even an int"
+
+		//when
+		isAny, err := index.IsAny(slice, item)
+
+		//then
+		require.Equal(t, nil, err)
+		assert.Equal(t, false, isAny)
+	})
+
+	t.Run("returns an error if first argument is not a slice", func(t *testing.T) {
+		//given
+		slice := "not a slice"
+		item := 1
+
+		//when
+		isAny, err := index.IsAny(slice, item)
+
+		//then
+		require.Equal(t, "index: given non-slice type", err.Error())
+		assert.Equal(t, false, isAny)
+	})
+}
+
+func TestIndex_HowMany(t *testing.T) {
+	t.Run("returns correct number of items in the slice", func(t *testing.T) {
+		//given
+		slice := []int{1, 2, 3, 1}
+		item := 1
+
+		//when
+		howMany, err := index.HowMany(slice, item)
+
+		//then
+		require.Equal(t, nil, err)
+		assert.Equal(t, 2, howMany)
+	})
+
+	t.Run("returns zero if there is no proper item in the slice", func(t *testing.T) {
+		//given
+		slice := []int{1, 2, 3, 1}
+		item := "not even an int"
+
+		//when
+		howMany, err := index.HowMany(slice, item)
+
+		//then
+		require.Equal(t, nil, err)
+		assert.Equal(t, 0, howMany)
+	})
+
+	t.Run("returns zero if the slice is empty", func(t *testing.T) {
+		//given
+		slice := []int{}
+		item := 1
+
+		//when
+		howMany, err := index.HowMany(slice, item)
+
+		//then
+		require.Equal(t, nil, err)
+		assert.Equal(t, 0, howMany)
+	})
+
+	t.Run("returns an error if first argument is not a slice", func(t *testing.T) {
+		//given
+		slice := "not a slice"
+		item := 1
+
+		//when
+		howMany, err := index.HowMany(slice, item)
+
+		//then
+		require.Equal(t, "index: given non-slice type", err.Error())
+		assert.Equal(t, 0, howMany)
+	})
 }
