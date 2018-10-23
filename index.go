7a9c85b1d7512693fbf8759f66e1a8d9fb3c6b66
franpog859 <franpog859@student.polsl.pl>
2018-10-23 13:12:15 +0200

    Change function parameter names due to Go convencion

--- index.go
+++ index.go
@@ -10,51 +10,51 @@ import (
 // GetAll function allows you to check on which indexes your item
 // is positioned in the slice. First argument is the slice in which
 // you are searching for your item and the second one is the item.
-func GetAll(_slice interface{}, _item interface{}) (indexes []int, err error) {
+func GetAll(givenSlice interface{}, givenItem interface{}) (indexes []int, err error) {
 	defer func() {
 		if recover() != nil {
 			err = errors.New("index: incorrect usage of the package")
 		}
 	}()
 
-	slice, err := sliceFromInterface(_slice)
+	slice, err := getSliceFromInterface(givenSlice)
 	if err != nil {
 		return []int{}, err
 	}
 
 	indexes = []int{}
-	for index, item := range slice {
+	for i, item := range slice {
 		if err := checkItemComplexity(item); err != nil {
-			return []int{}, err
+			return indexes, err
 		}
-		if item == _item {
-			indexes = append(indexes, index)
+		if item == givenItem {
+			indexes = append(indexes, i)
 		}
 	}
 
 	return indexes, nil
 }
 
-func sliceFromInterface(sliceInterface interface{}) (ret []interface{}, err error) {
-	slice := reflect.ValueOf(sliceInterface)
-	if slice.Kind() != reflect.Slice {
+func getSliceFromInterface(sliceInterface interface{}) (slice []interface{}, err error) {
+	value := reflect.ValueOf(sliceInterface)
+	if value.Kind() != reflect.Slice {
 		return nil, errors.New("index: given non-slice type")
 	}
 
-	ret = make([]interface{}, slice.Len())
-	for i := 0; i < slice.Len(); i++ {
-		ret[i] = slice.Index(i).Interface()
+	slice = make([]interface{}, value.Len())
+	for i := 0; i < value.Len(); i++ {
+		slice[i] = value.Index(i).Interface()
 	}
 
-	return ret, nil
+	return slice, nil
 }
 
-func checkItemComplexity(_item interface{}) (err error) {
-	item := reflect.ValueOf(_item)
-	if item.Kind() == reflect.Slice {
+func checkItemComplexity(item interface{}) (err error) {
+	value := reflect.ValueOf(item)
+	if value.Kind() == reflect.Slice {
 		return errors.New("index: given multidimensional slice")
 	}
-	if item.Kind() == reflect.Map {
+	if value.Kind() == reflect.Map {
 		return errors.New("index: given too complex slice of maps")
 	}
 
@@ -64,8 +64,8 @@ func checkItemComplexity(_item interface{}) (err error) {
 // IsAny function allows you to check if there is at least one item
 // in the slice that you gave. First argument is the slice in which
 // you are searching for your item and the second one is the item.
-func IsAny(_slice interface{}, _item interface{}) (isAny bool, err error) {
-	indexes, err := GetAll(_slice, _item)
+func IsAny(slice interface{}, item interface{}) (isAny bool, err error) {
+	indexes, err := GetAll(slice, item)
 	if err != nil {
 		return false, err
 	}
@@ -76,8 +76,8 @@ func IsAny(_slice interface{}, _item interface{}) (isAny bool, err error) {
 // HowMany function allows you to check how many items that you gave
 // exist in the slice. First argument is the slice in which
 // you are searching for your items and the second one is the item.
-func HowMany(_slice interface{}, _item interface{}) (howMany int, err error) {
-	indexes, err := GetAll(_slice, _item)
+func HowMany(slice interface{}, item interface{}) (howMany int, err error) {
+	indexes, err := GetAll(slice, item)
 	if err != nil {
 		return 0, err
 	}
